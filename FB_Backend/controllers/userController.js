c840fd71848a0d6759e19ebb5dc2e46841fb8d32
MVKarthikReddy <karthikmedagam@gmail.com>
2024-03-14 14:52:45 +0530

    user controller is added

---
+++ FB_Backend/controllers/userController.js
@@ -0,0 +1,74 @@
+const User = require('../models/userModel');
+const Review = require('../models/reviewModel');
+const Cart = require("../models/bagModel");
+
+// @desc Register the user
+// @route POST /api/users/register/
+// @access public
+
+const signup = async (req, res) => {
+    try {
+        let data = User(req.body);
+        let result = await data.save({ writeConcern: { w: 'majority' } });
+        console.log(result);
+        return res.status(200).send({message: 'User account created'});
+    } catch (error) {
+        if (error.code === 11000 && (error.keyPattern.email || error.keyPattern.phoneNo)) {
+            return res.status(400).send({message: 'User with this email or phone number already exists'});
+        }
+        console.log(req.body);
+        return res.status(500).send({message: 'Something went wrong!'});
+        // return res.status(500).send({message: error.message});
+
+    }
+}
+
+
+// @desc Login the user
+// @route POST /api/users/login/
+// @access public
+
+const login = async (req, res) => {
+    try {
+        let data = await User.findOne({ email: req.body.email, password: req.body.password });
+        if (!data) {
+            res.status(404);
+            return res.send({message: "User not found"});
+        }
+        else {
+            return res.status(200).send({message: "User login sucessful", userData: {_id:data._id, name: data.name, email: data.email, phoneNo: data.phoneNo}, isLoggedIn: true});
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({message: 'Something went wrong!'});
+    }
+}
+
+
+// @desc Delete the user
+// @route POST /api/users/delete/
+// @access private
+
+const deleteUserAccount = async (req, res) => {
+    try {
+         // Find and delete user data from other collections
+         await Promise.all([
+            Cart.deleteMany({ userId: req.params.userId }),
+            Review.deleteMany({ userId: req.params.userId  }),
+        ]);
+
+        let data = await User.deleteOne({ _id: req.params.userId }, { writeConcern: { w: 'majority' } });
+        res.status(200).send(data);
+
+    } catch (error) {
+        res.status(500).send('Something went wrong!');
+        console.log(error);
+    }
+}
+
+
+module.exports = {
+    signup,
+    login,
+    deleteUserAccount
+};
