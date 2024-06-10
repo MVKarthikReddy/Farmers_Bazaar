73120931862f67095ea9eaa5664e0119261c5077
MVKarthikReddy <karthikmedagam@gmail.com>
2024-06-10 22:00:35 +0530

    small changes

--- FB_Frontend/src/pages/home/Category.jsx
+++ FB_Frontend/src/pages/home/Category.jsx
@@ -8,37 +8,37 @@ function Category() {
       <Link to={"/category/rice"}>
         <CategoryCard
           title="Rice"
-          image="https://unsplash.com/photos/brown-wheat-in-close-up-photography-Y678onxFoJI"
+          image="https://img.etimg.com/thumb/msid-106097139,width-300,height-225,imgsize-907343,resizemode-75/rice-price-today.jpg"
         />
       </Link>
       <Link to={"/category/wheat"}>
         <CategoryCard
           title="Wheat"
-          image="https://unsplash.com/photos/wheat-field-y4xZxzN754M"
+          image="https://cdn.britannica.com/80/157180-050-7B906E02/Heads-wheat-grains.jpg"
         />
       </Link>
       <Link to={"/category/nuts"}>
         <CategoryCard
           title="Nuts"
-          image="https://unsplash.com/photos/variety-of-assorted-color-beans-UhrHTmVBzzE"
+          image="https://www.allrecipes.com/thmb/Wg9OMnGfdNUmqSVT_HhN9BX9OQY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/How-to-Store-Nuts-3x2-1-47cd3e4e561c466984fd00251a5d6f70.png"
         />
       </Link>
       <Link to={"/category/sugar"}>
         <CategoryCard
           title="Sugar"
-          image="https://unsplash.com/photos/spoon-of-powder-oKay0q7Pa30"
+          image="https://cdn.britannica.com/47/192347-131-62E67449/types-sugar-brown.jpg"
         />
       </Link>
       <Link to={"/category/spices"}>
         <CategoryCard
           title="Spices"
-          image="https://unsplash.com/photos/a-variety-of-spices-on-a-white-table-NPrWYa69Mz0"
+          image="https://hospitalityinsights.ehl.edu/hubfs/1440/1440x960-spices.jpg"
         />
       </Link>
       <Link to={"/category/fruits"}>
         <CategoryCard
           title="Fruits"
-          image="https://unsplash.com/photos/red-apple-fruit-beside-green-apple-and-yellow-fruit-on-brown-woven-basket-5JQH9Iqnm9o"
+          image="https://www.fruitsmith.com/pub/media/mageplaza/blog/post/o/n/one_seed_fruits.png"
         />
       </Link>
       <Link to={"/category/vegetables"}>
@@ -50,7 +50,7 @@ function Category() {
       <Link to={"/category/pulses"}>
         <CategoryCard
           title="Pulses"
-          image="https://unsplash.com/photos/a-close-up-of-a-snake-CswUmX6JTvg"
+          image="https://cdn.shopaccino.com/refresh/articles/dal-pulse-745535_l.jpg?v=421"
         />
       </Link>
     </div>
